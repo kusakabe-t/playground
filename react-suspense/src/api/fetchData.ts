a55e532bbf673d2bfd351685d0a4ab3f56af891f
kusakabe <kusakabe.takuya@alleeks.com>
2022-05-08 23:56:11 +0900

    update repository

--- react-suspense/src/api/fetchData.ts
+++ react-suspense/src/api/fetchData.ts
@@ -1,11 +1,38 @@
-import wrapPromise from './wrapPromise'
-
 const fetchData = (url: string) => {
 		const promise = fetch(url)
 				.then((res) => res.json())
 				.then((res) => res.data)
 
+		// return promise
 		return wrapPromise(promise)
+}
+
+const wrapPromise = (promise: Promise<any>) => {
+		let status = 'pending'
+		let response: any
+		const suspender = promise.then(
+				(res: any) => {
+						status = 'success'
+						response = res
+				},
+				(error: any) => {
+						status = 'error'
+						response = error
+				}
+		)
+
+		const read = () => {
+				switch (status) {
+						case 'pending':
+								throw suspender
+						case 'error':
+								throw response
+						default:
+								return response
+				}
+		}
+
+		return { read }
 }
 
 export default fetchData
